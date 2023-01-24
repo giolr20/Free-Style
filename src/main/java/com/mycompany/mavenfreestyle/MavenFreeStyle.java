4b335e42ef7acb31c049931e65abf69aedae1998
giolr20 <123190932+giolr20@users.noreply.github.com>
2023-01-24 20:03:46 +0000

    Update MavenFreeStyle.java

--- src/main/java/com/mycompany/mavenfreestyle/MavenFreeStyle.java
+++ src/main/java/com/mycompany/mavenfreestyle/MavenFreeStyle.java
@@ -1,26 +1,27 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
 
-package com.mycompany.mavenfreestyle;
+package com.mycompany.freestyle;
 
 /**
  *
- * @author olivert
+ * @author puk
  */
-public class MavenFreeStyle {
+public class Freestyle {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(fibonacci(4));
     }
+    
+    public static int fibonacci(int n){
    
 	if (n == 0) {
 		return 0;
-	} else if (n== 1)  {
+	} else if (n == 1)  {
 		return 1;
 	} else {
-		return fibonacciRecursivo(n - 1) + fibonacciRecursivo(n - 2);
+		return fibonacci(n - 1) + fibonacci(n - 2);
 	}
-   
+    }
 }
